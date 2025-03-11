0e3919ed3932ff3ee998b0139d562b5e351ead5d
angelasoler <manzanaembicicleta@gmail.com>
2025-03-11 12:47:00 -0300

    manual vector scalar operations

---
+++ ex03/ft_calculator.py
@@ -0,0 +1,31 @@
+class calculator:
+    def __init__(self, vector) -> None:
+        self.value = vector
+
+    """A simple calculator class"""
+    def __add__(self, object) -> None:
+        """Add two numbers together"""
+        for i in range(len(self.value)):
+            self.value[i] += object
+        print(self.value)
+
+    def __mul__(self, object) -> None:
+        """Multiply two numbers together"""
+        for i in range(len(self.value)):
+            self.value[i] *= object
+        print(self.value)
+
+    def __sub__(self, object) -> None:
+        """Subtract two numbers"""
+        for i in range(len(self.value)):
+            self.value[i] -= object
+        print(self.value)
+
+    def __truediv__(self, object) -> None:
+        """Divide two numbers"""
+        for i in range(len(self.value)):
+            try:
+                self.value[i] /= object
+            except ZeroDivisionError:
+                print("Division by zero is not allowed")
+        print(self.value)
---
+++ ex03/tester.py
@@ -0,0 +1,15 @@
+from ft_calculator import calculator
+
+# Create a calculator object with a list of numbers
+# Perform the following operations: +, *, -, /
+# Print the results
+v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+v1 + 5
+print("---")
+v2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+v2 * 5
+print("---")
+v3 = calculator([10.0, 15.0, 20.0])
+v3 - 5
+v3 / 5
+print("---")
